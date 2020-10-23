2efd5d97377395cfe9d48d1932d88a96ce4670dd
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-10-23 10:34:20 +0200

    Add latest code

--- js-23-10/index.js
+++ js-23-10/index.js
@@ -12,8 +12,14 @@
 
 var number1 = 10;
 var number2 = 5;
+var float = 10.5;
+var scientific = 1e3;
 var total = number1 + number2;
 
+console.log(float);
+console.log(scientific);
+console.log(typeof float);
+console.log(typeof scientific);
 console.log(number1);
 console.log(number2);
 console.log(total);
@@ -39,3 +45,71 @@ console.log(fullname + number1 + number2);
 var collin;
 console.log(collin);
 console.log(typeof collin);
+
+/* Boolean */
+var isOld = true;
+var isSexy = false;
+
+console.log(isOld);
+console.log(isSexy);
+console.log(typeof isOld);
+
+/* Object */
+var colors = ["orange", "pink", "green", 4];
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(typeof colors);
+
+var person = {
+    firstname: "Collin",
+    name: "Van der Vorst",
+    age: 29,
+    hasGoats: true,
+    chickens: [
+        "Chicken1",
+        "Chicken2",
+        "Chicken3"
+    ]
+}
+
+console.log(person);
+console.log(person.age + 1);
+console.log(typeof person);
+
+var attributeName = "age";
+console.warn(person["age"]);
+console.warn(person[attributeName]);
+
+var students = [
+    {
+        name: "Collin"
+    },
+    {
+        name: "Marnix"
+    }
+];
+
+console.log(students[0].name);
+
+var testArray = [
+    [
+        1,
+        2,
+        3
+    ],
+    [
+        4,
+        5,
+        6
+    ]
+];
+console.log(testArray);
+
+document.write("Oops, something went wrong");
+// alert("Popup");
+var promptName = prompt("Give me your name, fool");
+
+console.log(promptName);
