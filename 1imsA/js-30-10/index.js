9952ed6f6c86f99a8f62c61825afaa2336eba185
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-10-30 10:55:15 +0100

    Latest version of our lovely code

--- 1imsA/js-30-10/index.js
+++ 1imsA/js-30-10/index.js
@@ -21,16 +21,41 @@
 // console.log(document.getElementById("navigation").parentElement);
 // console.log(document.querySelectorAll("#navigation ul li")[0].parentElement);
 
-var heading1 = document.getElementById("heading1");
+// var heading1 = document.getElementById("heading1");
 
-heading1.innerText = "Collin"; // als tekstschouwen
-heading1.innerHTML = "Collin <span>Van der Vorst</span>"; // als html gaan aanschouwen
-// heading1.style.color = "green";
+// heading1.innerText = "Collin"; // als tekstschouwen
+// heading1.innerHTML = "Collin <span>Van der Vorst</span>"; // als html gaan aanschouwen
+// // heading1.style.color = "green";
 
-console.log(heading1.id);
-heading1.setAttribute("attribute", "attribute-value");
+// console.log(heading1.id);
+// heading1.setAttribute("attribute", "attribute-value");
 
-heading1.setAttribute("class", "heading-green"); // overschrijven
-// heading1.classList.add("heading-green"); // classe toevoegen
+// heading1.setAttribute("class", "heading-green"); // overschrijven
+// // heading1.classList.add("heading-green"); // classe toevoegen
 
-console.log(heading1);
+// console.log(heading1);
+
+// console.log(document.querySelectorAll(".heading2"));
+// document.querySelectorAll(".heading2")[1].innerHTML = "Rock Werchter";
+
+console.log(document.querySelector("#navigation ul"));
+
+var headingBottom = document.createElement("h2");
+headingBottom.innerHTML = "Toegevoegd op het einde";
+
+// // In de html tag op het einde gaan toevoegen
+// document.querySelectorAll("#placeholder p")[1].append(headingBottom); 
+
+// // In de html tag in het begin gaan toevoegen
+// document.querySelectorAll("#placeholder p")[1].prepend(headingBottom); 
+
+// // Na de html tag toevoegen
+// document.querySelectorAll("#placeholder p")[1].after(headingBottom);
+
+// Voor de html tag toevoegen
+// document.querySelectorAll("#placeholder p")[1].before(headingBottom);
+
+// document.querySelector("body").append("Toegevoegd op het einde");
+
+var nav = document.querySelector("#navigation");
+document.body.removeChild(nav);
