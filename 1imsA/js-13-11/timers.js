84254636f28cb5d244cdbf7d9ebf0eeb11c7d3a9
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-11-13 14:12:18 +0100

    Timers.html

--- 1imsA/js-13-11/timers.js
+++ 1imsA/js-13-11/timers.js
@@ -1,13 +1,33 @@
-function scream() {
-    console.log("scream");
+// function scream() {
+//     console.log("scream");
+// }
+
+// //  Timeout 
+// var showPopup = setTimeout(scream, 3000);
+
+// // document.querySelector("#js-stop-timeout").addEventListener("click", function() {
+// //     console.log("geklikt");
+// //     clearTimeout(showPopup);
+// // });
+
+// setInterval(scream, 1000);
+
+var second= 0;
+var chronometer;
+
+function logSecond() {
+    if (second < 5) {
+        second++;
+        document.querySelector("#heading1").innerHTML = second;
+    } else {
+        clearInterval(chronometer);
+    }
 }
 
-//  Timeout 
-var showPopup = setTimeout(scream, 3000);
+document.querySelector("#js-start").addEventListener("click", function() {
+    chronometer = setInterval(logSecond, 1000);
+});
 
-// document.querySelector("#js-stop-timeout").addEventListener("click", function() {
-//     console.log("geklikt");
-//     clearTimeout(showPopup);
-// });
-
-setInterval(scream, 1000);
+document.querySelector("#js-stop").addEventListener("click", function() {
+    clearInterval(chronometer);
+});
