99ce9acde64f0059db588b54fa5ea17e5d7b634b
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-11-13 15:01:33 +0100

    Add arrays

--- 1dsps/js-13-11/index.js
+++ 1dsps/js-13-11/index.js
@@ -1,10 +1,44 @@
-function changeColor(color) {
-    console.log("changeColor to ", color);
-    document.body.style.backgroundColor = color;
+// console.log(document);
+
+// document.getElementById(); // gives 1 object
+// document.getElementsByClassName(); // list of objects
+// document.getElementsByTagName; // list of ovbject
+// document.querySelectorAll(); // list of objects
+
+// var button = document.querySelector("#redButton"); // 1gives 1 object
+
+// button.addEventListener("click", function() {
+//     document.querySelector("body").style.backgroundColor = "red";
+// });
+
+// var buttonGreen = document.querySelector("#greenButton"); // 1gives 1 object
+
+// buttonGreen.addEventListener("click", function() {
+//     document.querySelector("body").style.backgroundColor = "green";
+// });
+function changeBodyColor(color) {
+    document.querySelector("body").style.backgroundColor = color;
 }
 
-document.querySelectorAll(".js-button")[0].addEventListener("click", changeColor("red"));
+function isWeatherNice() {
+    return false;
+}
 
-document.querySelectorAll(".js-button")[1].addEventListener("click", function() {
-    changeColor("green");
-});
+var howIstheWeather = isWeatherNice();
+
+var buttons = document.querySelectorAll(".js-button");
+
+for (var index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", function() {
+        changeBodyColor(this.innerHTML);
+    });
+}
+
+// console.log(window.outerHeight);
+// console.log(window.outerWidth);
+
+// window.history.back();
+
+console.log(window.buttons);
+console.log(this.buttons);
+console.log(buttons);
