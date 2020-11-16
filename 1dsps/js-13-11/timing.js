a95eb85a57630b822d3723822e8d1073192a6626
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-11-16 08:07:07 +0100

    Latst version

--- 1dsps/js-13-11/timing.js
+++ 1dsps/js-13-11/timing.js
@@ -1,13 +1,35 @@
-function sayHi() {
-    console.log("Hello you stanger");
+// function sayHi() {
+//     console.log("Hello you stanger");
+// }
+
+// var timeout = setTimeout(sayHi, 3000);
+
+// // document.querySelector("#stop_saying_hello").addEventListener("click", function() {
+// //     clearTimeout(timeout);
+// // });
+
+// // Set interval
+var counter = 0;
+var interval;
+
+function addSecond() {
+    console.log(counter);
+    if (counter >= 10) {
+        clearInterval(interval);
+    } else {
+        counter++;
+        document.querySelector("#heading1").innerHTML = counter;
+    }
 }
 
-var timeout = setTimeout(sayHi, 3000);
+document.querySelector("#js-start-chronometer").addEventListener("click", function() {
+    interval = setInterval(addSecond, 1000);
+});
 
-// document.querySelector("#stop_saying_hello").addEventListener("click", function() {
-//     clearTimeout(timeout);
-// });
+document.querySelector("#js-stop-chronometer").addEventListener("click", function() {
+    clearInterval(interval);
+});
 
-// Set interval
-
-var interval = setInterval(sayHi, 3000);
+document.querySelector("#js-reset-chronometer").addEventListener("click", function() {
+    counter = 0;
+});
