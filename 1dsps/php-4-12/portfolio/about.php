80909ecc6a97a0643c5506fb6c4aaf742ecbe0c0
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-12-04 15:14:42 +0100

    Portolio start

---
+++ 1dsps/php-4-12/portfolio/about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <!-- Header -->
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="./index.php">
+                            Home
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="./about.php">
+                            About
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="./contact.php">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Main content -->
+        <main>
+            <h1>
+                About
+            </h1>
+        </main>
+
+        <!-- Footer -->
+        <footer>
+            Made with love by Collin
+        </footer>
+    </body>
+</html>
