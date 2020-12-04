f41d80778f729cf104723dce33180165eff81ccf
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-12-04 15:18:23 +0100

    components

---
+++ 1dsps/php-4-12/portfolio/components/header.php
@@ -0,0 +1,29 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="./index.php">
+                    Home
+                </a>
+            </li>
+
+            <li>
+                <a href="./about.php">
+                    About
+                </a>
+            </li>
+
+            <li>
+                <a href="./contact.php">
+                    Contact
+                </a>
+            </li>
+
+            <li>
+                <a href="./projects.php">
+                    Projects
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
--- 1dsps/php-4-12/portfolio/index.php
+++ 1dsps/php-4-12/portfolio/index.php
@@ -7,29 +7,9 @@
     </head>
     <body>
         <!-- Header -->
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="./index.php">
-                            Home
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="./about.php">
-                            About
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="./contact.php">
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <?php
+            include "./components/header.php";
+        ?>
 
         <!-- Main content -->
         <main>
