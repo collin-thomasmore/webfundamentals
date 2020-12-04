b6af7cc30626ad4bc2ba34ed2977d598530f89f3
Collin Van der Vorst <collinvandervorst@mbp-van-collin.lan>
2020-12-04 09:49:53 +0100

    header php

---
+++ php-4-12/about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <!-- Header -->
+        <?php
+            include "components/header.php";
+        ?>
+
+        <!-- Main -->
+        <main>
+            <h1>
+                About
+            </h1>
+        </main>
+    </body>
+</html>
---
+++ php-4-12/components/header.php
@@ -0,0 +1,29 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="./index.php">
+                    Home pagina
+                </a>
+            </li>
+
+            <li>
+                <a href="./about.php">
+                    About pagina
+                </a>
+            </li>
+
+            <li>
+                <a href="./contact.php">
+                    Contact pagina
+                </a>
+            </li>
+
+            <li>
+                <a href="./projects.php">
+                    projects pagina
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
